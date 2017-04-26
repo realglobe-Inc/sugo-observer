7f4569032322167f70dd7b235f1090b11e71f188
Taka Okunishi <okunishinishi@gmail.com>
2017-04-26 14:18:26 +0900

    Exampleにasync/awaitを使った

--- example/example-usage.js
+++ example/example-usage.js
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
 
 /**
- * This is an example to use observer to observe cloud
+ * This is an example to use observer to observe hub
  */
 'use strict'
 
-const co = require('co')
 const sugoObserver = require('sugo-observer')
 
-co(function * () {
+async function tryExample () {
   let observer = sugoObserver((data) => {
     console.log('Data changed:', data)
     /* ... */
@@ -16,9 +15,11 @@ co(function * () {
     host: 'my-sugo-hub.example.com'
   })
 
-  yield observer.start() // Start observing
+  await observer.start() // Start observing
   /* ... */
-  yield new Promise((resolve) => setTimeout(resolve, 80000))
-  yield observer.stop() // Stop observing
-}).catch((err) => console.error(err))
+  await new Promise((resolve) => setTimeout(resolve, 80000))
+  await observer.stop() // Stop observing
+}
+
+tryExample().catch((err) => console.error(err))
 
